47ae86ba5453158261821c9a65c85e23b4149bde
James Paolantonio <james@Jamess-MacBook-Pro.local>
2014-08-28 20:23:33 -0400

    Initial Commit

---
+++ ZipLocateSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZipLocateSample
+//
+//  Created by James Paolantonio on 8/28/14.
+//  Copyright (c) 2014 James Paolantonio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZipLocateSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZipLocateSample
+//
+//  Created by James Paolantonio on 8/28/14.
+//  Copyright (c) 2014 James Paolantonio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
